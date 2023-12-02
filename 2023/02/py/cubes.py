19ffa79abe095289d732f562502bdbb08a558431
z0nk3r <throwawayva1995@gmail.com>
2023-12-02 00:59:56 -0500

    first refactor of day 2

--- 2023/02/py/cubes.py
+++ 2023/02/py/cubes.py
@@ -1,3 +1,31 @@
+def get_maxes(line):
+    red = green = blue = 0
+    games = line.split(":")[1].split(";")
+    for game in games:
+        for play in game.split(","):
+            cnt, col = play.split()
+            if col == "blue":
+                if int(cnt) > blue:
+                    blue = int(cnt)
+            elif col == "red":
+                if int(cnt) > red:
+                    red = int(cnt)
+            elif col == "green":
+                if int(cnt) > green:
+                    green = int(cnt)
+            
+            # if play.split()[1] == "blue":
+            #     if int(play.split()[0]) > blue:
+            #         blue = int(play.split()[0])
+            # elif play.split()[1] == "red":
+            #     if int(play.split()[0]) > red:
+            #         red = int(play.split()[0])
+            # elif play.split()[1] == "green":
+            #     if int(play.split()[0]) > green:
+            #         green = int(play.split()[0])
+
+    return red, green, blue
+
 def part1():
     with open("../input") as input:
         lines = [line.replace("\n", "") for line in input.readlines()]
@@ -6,31 +34,12 @@ def part1():
     green = 13
     blue = 14
     answer = 0
-    id_ctr = 0
     for line in lines:
-        id = line.split(":")[0].split(" ")[1]
-        games = line.split(":")[1].split(";")
-        for game in games:
-            
-            for play in game.split(","):
-                if play.split()[1] == "blue":
-                    blue -= int(play.split()[0])
-                elif play.split()[1] == "red":
-                    red -= int(play.split()[0])
-                elif play.split()[1] == "green":
-                    green -= int(play.split()[0])
-            print(f"{id}: {game = }")
-            print(f"{red = }{green = }{blue = }")
-            if (red >= 0 and green >= 0 and blue >= 0):
-                id_ctr += 1
-            red = 12
-            green = 13
-            blue = 14
-
-        if (id_ctr == len(games)):
-            answer += int(id)
+        id = int(line.split(":")[0].split(" ")[1])
         
-        id_ctr = 0
+        r, g, b = get_maxes(line)
+        if (r <= red and g <= green and b <= blue):
+            answer += id
 
     print(f"1: {answer = }")
 
@@ -38,32 +47,11 @@ def part2():
     with open("../input") as input:
         lines = [line.replace("\n", "") for line in input.readlines()]
         
-    red = green = blue = 0
     answer = 0
     for line in lines:
-        id = line.split(":")[0].split(" ")[1]
-        print(f"================= {id} ==================")
-        games = line.split(":")[1].split(";")
-        for game in games:
-            for play in game.split(","):
-                if play.split()[1] == "blue":
-                    if int(play.split()[0]) > blue:
-                        blue = int(play.split()[0])
-                elif play.split()[1] == "red":
-                    if int(play.split()[0]) > red:
-                        red = int(play.split()[0])
-                elif play.split()[1] == "green":
-                    if int(play.split()[0]) > green:
-                        green = int(play.split()[0])
-            print(f"{id}: {game = }")
-            print(f"{red = } {green = } {blue = }")
-            
-            power = (red * green * blue)
-            
-        
+        r, g, b = get_maxes(line)
+        power = r * g * b
         answer += power
-        print(f"{id}: {power = }")
-        red = green = blue = 0
     
     print(f"2: {answer = }")
 
