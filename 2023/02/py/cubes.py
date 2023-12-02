fc74c4062265bfbbdeb36095f30c17c3a0e666cd
z0nk3r <throwawayva1995@gmail.com>
2023-12-02 01:34:01 -0500

    second refactor of day 2

--- 2023/02/py/cubes.py
+++ 2023/02/py/cubes.py
@@ -1,28 +1,15 @@
-def get_maxes(line):
+def get_maxes(games):
     red = green = blue = 0
-    games = line.split(":")[1].split(";")
     for game in games:
         for play in game.split(","):
             cnt, col = play.split()
-            if col == "blue":
-                if int(cnt) > blue:
-                    blue = int(cnt)
-            elif col == "red":
-                if int(cnt) > red:
-                    red = int(cnt)
-            elif col == "green":
-                if int(cnt) > green:
-                    green = int(cnt)
-            
-            # if play.split()[1] == "blue":
-            #     if int(play.split()[0]) > blue:
-            #         blue = int(play.split()[0])
-            # elif play.split()[1] == "red":
-            #     if int(play.split()[0]) > red:
-            #         red = int(play.split()[0])
-            # elif play.split()[1] == "green":
-            #     if int(play.split()[0]) > green:
-            #         green = int(play.split()[0])
+            cnt = int(cnt)
+            if col == "blue" and cnt > blue:
+                blue = cnt
+            elif col == "red" and cnt > red:
+                red = cnt
+            elif col == "green" and cnt > green:
+                green = cnt
 
     return red, green, blue
 
@@ -35,24 +22,21 @@ def part1():
     blue = 14
     answer = 0
     for line in lines:
-        id = int(line.split(":")[0].split(" ")[1])
-        
-        r, g, b = get_maxes(line)
+        id = int(line.split(": ")[0].split(" ")[1])
+        r, g, b = get_maxes(line.split(": ")[1].split(";"))  # [games]
         if (r <= red and g <= green and b <= blue):
             answer += id
-
     print(f"1: {answer = }")
 
 def part2():
     with open("../input") as input:
         lines = [line.replace("\n", "") for line in input.readlines()]
-        
+
     answer = 0
     for line in lines:
-        r, g, b = get_maxes(line)
+        r, g, b = get_maxes(line.split(": ")[1].split(";"))  # [games]
         power = r * g * b
         answer += power
-    
     print(f"2: {answer = }")
 
 if __name__ == "__main__":
