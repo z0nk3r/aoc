e49671db0b7947ee798884696ff8562388642335
z0nk3r <throwawayva1995@gmail.com>
2023-12-02 02:24:20 -0500

    final refactor day 2

--- 2023/02/py/cubes.py
+++ 2023/02/py/cubes.py
@@ -1,37 +1,23 @@
 def get_maxes(games):
-    red = green = blue = 0
+    maxes = {"red": 0, "green": 0, "blue": 0}
     for game in games:
         for play in game.split(","):
             cnt, col = play.split()
-            cnt = int(cnt)
-            if col == "blue" and cnt > blue:
-                blue = cnt
-            elif col == "red" and cnt > red:
-                red = cnt
-            elif col == "green" and cnt > green:
-                green = cnt
+            if int(cnt) > maxes[col]:
+                maxes[col] = int(cnt)
 
-    return red, green, blue
+    return maxes["red"], maxes["green"], maxes["blue"]
 
-def part1():
-    with open("../input") as input:
-        lines = [line.replace("\n", "") for line in input.readlines()]
-        
-    red = 12
-    green = 13
-    blue = 14
+def part1(lines):
     answer = 0
     for line in lines:
         id = int(line.split(": ")[0].split(" ")[1])
         r, g, b = get_maxes(line.split(": ")[1].split(";"))  # [games]
-        if (r <= red and g <= green and b <= blue):
+        if (r <= 12 and g <= 13 and b <= 14):
             answer += id
     print(f"1: {answer = }")
 
-def part2():
-    with open("../input") as input:
-        lines = [line.replace("\n", "") for line in input.readlines()]
-
+def part2(lines):
     answer = 0
     for line in lines:
         r, g, b = get_maxes(line.split(": ")[1].split(";"))  # [games]
@@ -40,5 +26,7 @@ def part2():
     print(f"2: {answer = }")
 
 if __name__ == "__main__":
-    part1()
-    part2()
+    with open("../input") as input:
+        lines = [line.replace("\n", "") for line in input.readlines()]
+    part1(lines)
+    part2(lines)
