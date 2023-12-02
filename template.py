637788ce127c4d108e8184671d62816d3563cdc4
z0nk3r <throwawayva1995@gmail.com>
2023-12-02 11:20:15 -0500

    template typo

--- template.py
+++ template.py
@@ -7,7 +7,7 @@ def part2(lines):
         pass
 
 if __name__ == "__main__":
-    with open("../input") as input:
+    with open("input") as input:
         lines = [line.replace("\n", "") for line in input.readlines()]
     part1(lines)
     part2(lines)
