671c274dd13f30d371f1a1034c577982b6707ead
z0nk3r <joshua.d.kaplan5@cybbh.space>
2023-12-06 10:21:36 -0500

    improved quadratic formula

--- 2023/06/wait_smol.py
+++ 2023/06/wait_smol.py
@@ -1,14 +1,11 @@
-from math import sqrt, floor, ceil
-def quadr(a, b, c):
-    quad1 = (-b + -sqrt(b*b - (4*a*c)))/(2*a)
-    quad2 = (-b - -sqrt(b*b - (4*a*c)))/(2*a)
-    if quad1 > quad2:
-        return floor(quad1) - ceil(quad2) + 1
-    else:
-        return floor(quad2) - ceil(quad1) + 1
+from math import floor, ceil
+
+def quadr(b, c):
+    lower = int((-b + (b*b - 4*c) ** 0.5) // -2)
+    return b - lower - lower - 1
 
 def main():
-    lines = [line.replace("\n", "") for line in open("input").readlines()]
+    lines = [line.replace("\n", "") for line in open(0).readlines()]
 
     times = list(map(int, lines[0].split(":")[1].split()))
     time = int("".join(lines[0].split(":")[1].split()))
@@ -17,11 +14,10 @@ def main():
 
     ans1 = 1
     for i in range(len(times)):
-        # ans1 *= sum([1 for t in range(times[i]) if (t*(times[i]-t)) > dists[i]])
-        ans1 *= quadr(1, -times[i], dists[i])
+        ans1 *= quadr(-times[i], dists[i])
     print(f"1: {ans1}")
 
-    ans2 = quadr(1, -time, dist)
+    ans2 = quadr(-time, dist)
     print(f"2: {ans2}")
 
 if __name__ == "__main__":
