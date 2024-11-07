790a49acc53e456e192eb111b5129f43a1624cd1
z0nk3r <throwawayva1995@gmail.com>
2024-11-06 21:12:50 -0500

    reworked logic in main for an auto function; placeholders for future features; formatting

--- aoc_get_input.py
+++ aoc_get_input.py
@@ -79,6 +79,20 @@ def get_input(year, day):
             print("Bad Puzzle Input Get - Try Again Later")
             break
 
+def setup_env(year, day):
+    # [ ] TODO: Setup Working Environment Automagically with VSCode
+    dest_filepath = f'{year}/{day:02}/'
+    # os.copy template down to dest_filepath
+    
+    
+    # rename template to input challengename
+    
+    
+    # open vscode at .
+    
+    
+    #open dest_filepath/challengename with `code -r`
+
 def time_to_release(year, day):
     # Puzzles release at midnight EST (UTC-5)
     release_time = datetime.datetime(year=year,
@@ -97,8 +111,8 @@ def print_countdown(to_wait_seconds):
     print(f'\r [X] {days:4}D, {hrs:02}H, {mins:02}M, {secs:02}S until Puzzle is Available. Waiting... ', end="", flush=True)
 
 def download_input_when_live(year, day):
-    # Download 4 seconds after release
-    to_wait = time_to_release(year, day) + datetime.timedelta(seconds=4)
+    # Download 2 seconds after release
+    to_wait = time_to_release(year, day) + datetime.timedelta(seconds=2)
     to_wait_seconds = int(to_wait.total_seconds())
 
     while to_wait_seconds > 0:
@@ -106,31 +120,58 @@ def download_input_when_live(year, day):
         to_wait_seconds -= 1
         print_countdown(to_wait_seconds)
         
+        # recalc the waittime every so often to account for potential drift of time
         if (to_wait_seconds % 300) == 0:  # every 5 minutes
-            # repeat these here for potential drift of time
-            to_wait = time_to_release(year, day) + datetime.timedelta(seconds=4)
+            to_wait = time_to_release(year, day) + datetime.timedelta(seconds=2)
             to_wait_seconds = int(to_wait.total_seconds())
 
     print(f'\nDownloading Puzzle Input for {year} Day {day} to ', end="")
     print(f'\"{os.getcwd()}/{year}/{day:02}/input\"')
     get_input(year, day)
+    print(f'Setting up environment for challenge in {os.getcwd()}/{year}/{day:02}/')
+    setup_env(year, day)
+
+def autocalc_yearday():
+    # [ ] TODO: Using the Datetime.datetime lib, auto figure out year and day of next challenge.
+    # If its before Dec 1st , return current year, and 1
+    # if its after dec 25th, return next year and 1
+    return 2024, 1
 
 def main():
-    if len(sys.argv) != 3:
-        print("Not all arguments provided")
+    year = -1
+    day = -1
+
+    try:
+        if sys.argv[1] == '-a':
+            year, day = autocalc_yearday()
+        elif len(sys.argv) != 3:
+            print("[x] Not all or too many arguments provided")
+            print("\nUsage:\n\n  $> python3 aoc_get_input.py <year> <day>\n")
+            return
+    except IndexError:
+        print("[x] Not all or too many arguments provided")
         print("\nUsage:\n\n  $> python3 aoc_get_input.py <year> <day>\n")
         return
 
-    year = int(sys.argv[1])
-    day = int(sys.argv[2])
-    if (2030 > year > 2015 and 0 < day < 26):
-        try:
-            download_input_when_live(year, day)
-        except KeyboardInterrupt:
-            print("\nQuitting!")
-    else:
-        print(f"Invalid Year Day Arguments: {year} {day}")
+    try:
+        if year == -1 and day == -1:
+            year = int(sys.argv[1])
+            day = int(sys.argv[2])
+
+        if (2030 > year > 2015 and 0 < day < 26):
+            try:
+                download_input_when_live(year, day)
+            except KeyboardInterrupt:
+                print("\nQuitting!")
+        else:
+            print(f"[x] Invalid Year or Day Given: {year} {day}")
+            print("[x] Must be in year range 2015 - 2030, and in the day range of the 1st - 25th")
+            print("\nUsage:\n\n  $> python3 aoc_get_input.py <year> <day>\n")
+
+    except ValueError:
+        print(f"[x] Invalid Arguments provided: {sys.argv[1]} {sys.argv[2]}")
         print("\nUsage:\n\n  $> python3 aoc_get_input.py <year> <day>\n")
+
 
 if __name__ == "__main__":
     main()
