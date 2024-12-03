91b038955d18660d879ef716fd9d36a5581188ea
z0nk3r <throwawayva1995@gmail.com>
2024-12-03 06:57:06 -0500

    day03 solved

---
+++ 2024/03/day03.py
@@ -0,0 +1,45 @@
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent.parent.parent))
+
+from lib import puzzle_setup, puzzle_run
+
+import re
+
+def part1(lines):
+    answer = 0
+    
+    for line in lines:
+        muls = re.findall("mul\(\d+,\d+\)", line)
+        for mul in muls:
+            nums = re.findall('\d+', mul)
+            answer += int(nums[0]) * int(nums[1])
+
+    return answer
+
+
+def part2(lines):
+    answer = 0
+    do = True
+    
+    for line in lines:
+        founds = re.findall("mul\(\d+,\d+\)|do\(\)|don\'t\(\)", line)
+        for res in founds:
+            if res == "don't()":
+                do = False
+            elif res == "do()":
+                do = True
+            else:
+                if do:
+                    nums = re.findall('\d+', res)
+                    answer += int(nums[0]) * int(nums[1])
+
+    return answer
+
+
+if __name__ == "__main__":
+    year, day = puzzle_setup()
+
+    lines = [line.replace("\n", "") for line in open(0).readlines()]
+
+    puzzle_run(part1, part2, lines, year, day)
