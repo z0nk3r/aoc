b4f619bb5f7b4e128803cf1d2ff5ccc8b5e495cc
z0nk3r <throwawayva1995@gmail.com>
2024-12-16 00:54:26 -0500

    day15 part2 solved; refactored from recursive to move queue

--- 2024/15/day15.py
+++ 2024/15/day15.py
@@ -4,32 +4,17 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent.parent))
 
 from lib import puzzle_run
+from time import sleep
 
 
-def print_maze(maze):
+def print_maze(move, max_moves, maze):
+    fps = 300
+    print("\033[2J\033[;H")
+    print(f"{move} of {max_moves}")
     for row in maze:
         print(' '.join(row))
-    return ''
+    sleep(1/fps)
 
-
-def handle_move(char, move, location, maze):
-    dirs = {'^': (-1, 0), '>': (0, 1), 'v': (1, 0), '<': (0, -1)}
-    c_dir = dirs[move]
-    new_r = location[0] + c_dir[0]
-    new_c = location[1] + c_dir[1]
-    if maze[new_r][new_c] == '.':
-        pass
-    elif maze[new_r][new_c] == '#':
-        return False, location
-    elif maze[new_r][new_c] == 'O':
-        status, loc = handle_move('O', move, (new_r, new_c), maze)
-        if not status:
-            return False, location
-
-    maze[location[0]][location[1]] = '.'
-    maze[new_r][new_c] = char
-    loc = (new_r, new_c)
-    return True, loc
 
 def part1(lines):
     '''Function to solve part 1'''
@@ -38,6 +23,7 @@ def part1(lines):
     maze = []
     moves = ''
     bot = (-1, -1)
+    dirs = {'^': (-1, 0), '>': (0, 1), 'v': (1, 0), '<': (0, -1)}
 
     for lidx, line in enumerate(lines):
         if line == '':
@@ -49,8 +35,39 @@ def part1(lines):
             if maze[ridx][cidx] == '@':
                 bot = (ridx, cidx)
 
-    for move in moves:
-        _, bot = handle_move('@', move, bot, maze)
+    print_maze(0, len(moves), maze)
+    for midx, move in enumerate(moves):
+        dir_r, dir_c = dirs[move]
+        moves_todo = []
+        moves_todo.append(bot)
+        do_move = True
+
+        for mov_r, mov_c in moves_todo:
+            new_r = mov_r + dir_r
+            new_c = mov_c + dir_c
+            if (new_r, new_c) in moves_todo:
+                continue
+            n_char = maze[new_r][new_c]
+            if n_char == '#':
+                do_move = False
+                break
+            if n_char == "O":
+                moves_todo.append((new_r, new_c))
+
+        if not do_move:
+            continue
+
+        maze[bot[0]][bot[1]] = '.'
+        maze[bot[0] + dir_r][bot[1] + dir_c] = '@'
+
+        for mov_r, mov_c in moves_todo[1:]:
+            maze[mov_r][mov_c] = '.'
+        for mov_r, mov_c in moves_todo[1:]:
+            maze[mov_r + dir_r][mov_c + dir_c] = 'O'
+
+        bot = (bot[0] + dir_r, bot[1] + dir_c)
+        print_maze(midx, len(moves), maze)
+    print_maze(len(moves), len(moves), maze)
 
     # answer
     for ridx, row in enumerate(maze):
@@ -65,11 +82,67 @@ def part2(lines):
     '''Function to solve part 2'''
     answer = 0
 
-    for line in lines:
-        print(line)
+    maze = []
+    moves = ''
+    bot = (-1, -1)
+    dirs = {'^': (-1, 0), '>': (0, 1), 'v': (1, 0), '<': (0, -1)}
 
-    # solve part 2 of the problem here
-    # answer = <the answer to the problem>
+    for lidx, line in enumerate(lines):
+        if line == '':
+            maze = lines[:lidx]
+            moves = ''.join(lines[lidx+1:])
+    for ridx, row in enumerate(maze):
+        row = row.replace('#', '##').replace('O', '[]').replace('.', '..').replace('@', '@.')
+        maze[ridx] = list(row)
+        for cidx, col in enumerate(row):
+            if maze[ridx][cidx] == '@':
+                bot = (ridx, cidx)
+
+    print_maze(0, len(moves), maze)
+    for midx, move in enumerate(moves):
+        dir_r, dir_c = dirs[move]
+        moves_todo = []
+        moves_todo.append(bot)
+        do_move = True
+        maze_bak = [list(row) for row in maze]
+
+        for mov_r, mov_c in moves_todo:
+            new_r = mov_r + dir_r
+            new_c = mov_c + dir_c
+            if (new_r, new_c) in moves_todo:
+                continue
+            n_char = maze[new_r][new_c]
+            if n_char == '#':
+                do_move = False
+                break
+            if n_char == "[":
+                moves_todo.append((new_r, new_c))
+                moves_todo.append((new_r, new_c + 1))
+            if n_char == "]":
+                moves_todo.append((new_r, new_c))
+                moves_todo.append((new_r, new_c - 1))
+
+        if not do_move:
+            continue
+
+        maze[bot[0]][bot[1]] = '.'
+        maze[bot[0] + dir_r][bot[1] + dir_c] = '@'
+
+        for mov_r, mov_c in moves_todo[1:]:
+            maze[mov_r][mov_c] = '.'
+        for mov_r, mov_c in moves_todo[1:]:
+            maze[mov_r + dir_r][mov_c + dir_c] = maze_bak[mov_r][mov_c]
+        
+        bot = (bot[0] + dir_r, bot[1] + dir_c)
+        print_maze(midx, len(moves), maze)
+    print_maze(len(moves), len(moves), maze)
+
+    # answer
+    for ridx, row in enumerate(maze):
+        for cidx, col in enumerate(row):
+            if maze[ridx][cidx] == '[':
+                answer += 100 * ridx + cidx
+
     return answer
 
 
